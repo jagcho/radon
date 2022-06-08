4933cd5a91cdc524f4005be945c9afe5619bce0f
jagcho <jagadishchodry@gmail.com>
2022-06-08 06:46:28 +0530

    assignment/author_id completed

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,4 +1,4 @@
-const { count } = require("console")
+const authorModel=require("../models/authorModel")
 const BookModel= require("../models/bookModel")
 
 const createBook= async function (req, res) {
@@ -7,14 +7,38 @@ const createBook= async function (req, res) {
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
+const authorBook= async function (req, res) {
+    let data= req.body
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find( {authorName : "HO" } )
-    console.log(allBooks)
-    if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
-    else res.send({msg: "No books found" , condition: false})
+    let savedData= await authorModel.create(data)
+    res.send({msg: savedData})
 }
+const getBooksbyChetanBhagat= async function (req, res) {
+    let data=await authorModel.find({author_name:"Chetan Bhagat"}).select({author_id:1,_id:0})
+    console.log(data)
+    let booklist= await BookModel.find({author_id:data.author_id})
+    res.send({msg: booklist})
+}
+const authorOfBook= async function (req, res) {
+    let data= await BookModel.findOneAndUpdate({name:"Two states"},{$set:{prices:100}},{new:true})
+    let authordata=await authorModel.find({author_id:data[0].author_id}).select({author_name :1, _id:0})
+    let price=data.prices
+    res.send({msg: authordata,price})
+}
+// const getBooksData= async function (req, res) {
+//     let allBooks= await BookModel.find( {authorName : "HO" } )
+//     console.log(allBooks)
+//     if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
+//     else res.send({msg: "No books found" , condition: false})
+// }
 
+const booksBetween50_100= async function (req, res) {
+    
+ let books= await BookModel.find( { price : { $gte: 50, $lte: 100} }).select({author_id :1, _id:0})
+ let authorname= await authorModel.find({author_id:books.map(x=>x.author_id)}).select({author_name :1,_id:0})
+console.log(authorname)
+res.send({msg:authorname})
+}
 
 const updateBooks= async function (req, res) {
     let data = req.body // {sales: "1200"}
@@ -45,6 +69,7 @@ const deleteBooks= async function (req, res) {
 
 
 
+
 // CRUD OPERATIONS:
 // CREATE
 // READ
@@ -57,3 +82,7 @@ module.exports.createBook= createBook
 module.exports.getBooksData= getBooksData
 module.exports.updateBooks= updateBooks
 module.exports.deleteBooks= deleteBooks
+module.exports.authorBook=authorBook
+module.exports.getBooksbyChetanBhagat=getBooksbyChetanBhagat
+module.exports.authorOfBook=authorOfBook
+module.exports.booksBetween50_100=booksBetween50_100
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+//const userController=require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
+//router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+//router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook  )
-
-router.get("/getBooksData", BookController.getBooksData)
-
+router.post("/authorBook", BookController.authorBook ) 
+router.get("/getBooksbyChetanBhagat", BookController.getBooksbyChetanBhagat)
+router.get("/authorOfBook", BookController.authorOfBook)
+router.get("/booksBetween50_100",BookController.booksBetween50_100)
 router.post("/updateBooks", BookController.updateBooks)
 router.post("/deleteBooks", BookController.deleteBooks)
 
