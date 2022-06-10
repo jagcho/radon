db31fff96b1e3655ad8612d0edf5e23fef26a832
jagcho <jagadishchodry@gmail.com>
2022-06-10 23:14:19 +0530

    completed assignment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
-const expressip= require('express-ip');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,19 +25,29 @@ app.use (
 //      app.get('/',function(res,req){
 //         console.log(req.ipInfo)
 //      })
-app.use('/', (req,res,next)=>{
-const ip= req.headers['x-forwarded-for'] || req.socket.localAddress;
-console.log(ip)
-next()
-    });
+// app.use('/', (req,res,next)=>{
+// const ip= req.headers['x-forwarded-for'] || req.socket.localAddress;
+// console.log(ip)
+// next()
+//     });
+
+
+const assignmentMw=function(req,res,next){
+    var currentdate=new Date();
+    var datetime = currentdate.getDate() + " "
+                  +(currentdate.getMonth()+1)+ " "
+                  +currentdate.getFullYear()+ " "
+                  +currentdate.getHours()+ ":"
+                  +currentdate.getMinutes()+ ":"
+                  +currentdate.getSeconds();
+   let ip =req.ip
+  let url=req.originalUrl
+   console.log( `${datetime} ${ip} ${url}`)
+   next();
+}
+app.use(assignmentMw)
+
 app.use('/', route);
-
-app.use('/admin',(req,res,next)=> {
-    console.log(req.baseUrl+req.path)
-    next()
-})
-
-
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
