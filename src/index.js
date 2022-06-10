e4358ca3039b645501004e3d7fff195f235163e9
jagcho <jagadishchodry@gmail.com>
2022-06-10 19:21:27 +0530

    assignmebt

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const expressip= require('express-ip');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,8 +21,21 @@ app.use (
         next();
   }
   );
-
+// app.use(expressip().getIpInfoMiddleware);
+//      app.get('/',function(res,req){
+//         console.log(req.ipInfo)
+//      })
+app.use('/', (req,res,next)=>{
+const ip= req.headers['x-forwarded-for'] || req.socket.localAddress;
+console.log(ip)
+next()
+    });
 app.use('/', route);
+
+app.use('/admin',(req,res,next)=> {
+    console.log(req.baseUrl+req.path)
+    next()
+})
 
 
 app.listen(process.env.PORT || 3000, function () {
